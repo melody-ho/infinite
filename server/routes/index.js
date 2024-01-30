5082ac4ab5665f994b401340ba0ef335aea76fa3
Melody Ho <hyho.melody@gmail.com>
2024-01-30 08:50:27 -0500

    Route all paths to index

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", (req, res) => {
-  res.sendFile("main.html", { root: "../client/dist/" });
+router.get("*", (req, res) => {
+  res.sendFile("index.html", { root: "../client/dist/" });
 });
 
 module.exports = router;
